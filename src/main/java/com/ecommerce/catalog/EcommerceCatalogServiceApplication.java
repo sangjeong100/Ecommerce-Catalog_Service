198d17dc74429dbadcd04dcdfd2e2db5098da9e6
sjyu <sjyu@i-leaders.co.kr>
2023-11-27 20:40:02 +0900

    feat : feignClient 추가

--- src/main/java/com/ecommerce/catalog/EcommerceCatalogServiceApplication.java
+++ src/main/java/com/ecommerce/catalog/EcommerceCatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EcommerceCatalogServiceApplication {
 
 	public static void main(String[] args) {
